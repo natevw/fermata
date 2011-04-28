627278cec57e76309804e1d1808e8093256aabe3
Nathan Vander Wilt <nate@andyet.net>
2011-04-28 10:59:28 -0700

    add some basic tests to start with

---
+++ test.js
@@ -0,0 +1,29 @@
+f = require("./fermata.js");
+assert = require('assert');
+
+f.api({url:"http://pdxapi.com/"}).bicycle_parking.geojson({bbox:"-122.6785969,45.5167974,-122.6763439,45.51772964"})
+
+u = f.api({url:"http://example.com"});
+assert.equal(u(), "http://example.com/");
+assert.equal(u.folder(), "http://example.com/folder");
+assert.equal(u.folder1.folder2(), "http://example.com/folder1/folder2");
+
+u = f.api({url:"dotcom"});
+assert.equal(u({q:"search term"})(), "dotcom/?q=search%20term");
+assert.equal(u({opt:[0,1,2]})(), "dotcom/?opt=0&opt=1&opt=2");
+assert.equal(u({$key:[1,2,3]})(), "dotcom/?key=%5B1%2C2%2C3%5D");
+assert.equal(u({$$opt:[1,'two',3]})(), "dotcom/?%24opt=1&%24opt=two&%24opt=3");
+
+u = f.api({url:"site"});
+assert.equal(u.path.subpath({q1:"search", q2:"term"})(), "site/path/subpath?q1=search&q2=term");
+assert.equal(u.path({q1:"search", q2:"term"}).subpath(), "site/path/subpath?q1=search&q2=term");
+assert.equal(u.path({q1:"old", q2:"term"}).subpath({q1:"search"})(), "site/path/subpath?q1=search&q2=term");
+
+// actual over-the-network test. (won't notice if fermata simply never calls back; whines if NON-fermata stuff is down...)
+f.api({url:"http://pdxapi.com/"}).bicycle_parking.geojson({bbox:"-122.6785969,45.5167974,-122.6763439,45.51772964", limit:5}).get(function (e, o) {
+    console.log(e, o);
+    assert.ifError(e);
+    assert.ok(o.update_seq);
+    assert.ok(o.rows.length <= 5);
+});
+
