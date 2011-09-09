040ca8a8d6278cd0702d486bd9fddb9f2bbf47fe
Nathan Vander Wilt <nate@andyet.net>
2011-09-09 15:23:43 -0700

    cleaned up twitter plugin to simply use chaining

--- plugins/twitter.js
+++ plugins/twitter.js
@@ -1,51 +1,12 @@
-var oauth = require('./oauth');
+// for https://dev.twitter.com/docs/api
 
-function _formEncode(data) {
-    // see http://www.w3.org/TR/1998/REC-html40-19980424/interact/forms.html#h-17.13.4.1 and
-    // http://www.w3.org/TR/html5/association-of-controls-and-forms.html#application-x-www-form-urlencoded-encoding-algorithm
-    return oauth.listQuery(data).map(function (kv) {
-        return encodeURIComponent(kv[0]).replace(/%20/g, '+') + '=' + encodeURIComponent(kv[1]).replace(/%20/g, '+');
-    }).join("&");
-}
-
-function twitterPlugin(transport, cred) {    // credentials = {client, client_secret, token, token_secret}
+module.exports = function twitterPlugin(transport, oauth_cred) {
     this.base = "https://api.twitter.com";
-    return function (req, cb) {
+    transport = transport.using('statusCheck').using('autoConvert', "application/json").using('oauth', oauth_cred);
+    
+    return function (req, callback) {
         req.path[req.path.length - 1] += ".json";
-        req.headers['Accept'] = "application/json";
         req.headers['Content-Type'] = "application/x-www-form-urlencoded";
-        req.headers['Authorization'] = oauth.authorizeHMAC(req, cred);
-        req.data = req.data && _formEncode(req.data);
-        transport(req, function (err, response) {
-            if (!err) {
-                if (response.status.toFixed()[0] !== '2') {
-                    err = Error("Bad status code from server: " + response.status);
-                }
-                try {
-                    response = JSON.parse(response.data);
-                } catch (e) {
-                    err = e;
-                }
-            }
-            cb(err, response);
-        });
+        transport(req, callback);
     };
-}
-
-exports.plugin = twitterPlugin;
-
-
-/*
-var f = require('./fermata'),
-    t = require('./plugins/twitter');
-f.registerPlugin('twitter', t.plugin);
-
-var twebz = f.twitter({client:"", client_secret:"", token:"", token_secret:""});
-
-twebz.statuses.home_timeline.get(function (e, d) {
-    console.log(e,d);
-});
-
-twebz.statuses.update.post({status:"FRIST TWEET from Fermata!!1! (Using new OAuth stuff, in progress on https://github.com/andyet/fermata/tree/plugins branch)"}, function (e,d) {console.log(e,d);})
-
-*/
+};
